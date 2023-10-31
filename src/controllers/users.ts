7f2e467f83c9242e5e2e359d84001bd824cd0274
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-31 16:02:00 +0530

    new controller added for delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { getUsers } from '../db/user'
+import { deleteUserById, getUsers } from '../db/user'
 
 export const getAllUsers = async (req: Request, res: Response) => {
 	try {
@@ -10,6 +10,22 @@ export const getAllUsers = async (req: Request, res: Response) => {
 		console.log(error)
 		return res.sendStatus(400).json({
 			success: false,
+			message: 'Something went wrong!'
+		})
+	}
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params
+
+		const deletedUser = await deleteUserById(id)
+
+		return res.json(deletedUser)
+	} catch (error) {
+		console.log(error)
+		return res.sendStatus(400).json({
+			success: true,
 			message: 'Something went wrong!'
 		})
 	}
