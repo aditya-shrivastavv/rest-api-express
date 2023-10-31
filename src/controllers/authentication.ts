21a8cc58f38e7acafe4fcb50234f40547bccde01
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-31 15:28:25 +0530

    added controller and router

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
-import { createUser, getUserByEmail } from 'db/user'
+import { createUser, getUserByEmail } from '../db/user'
 import { Request, Response } from 'express'
-import { random } from 'helpers'
+import { authentication, random } from '../helpers/index'
 
 export const register = async (req: Request, res: Response) => {
 	try {
@@ -34,6 +34,56 @@ export const register = async (req: Request, res: Response) => {
 		return res
 			.status(200)
 			.json({ success: true, message: 'User registered successfully.', user })
+	} catch (error) {
+		console.log(error)
+		return res.sendStatus(400).json({
+			success: false,
+			message: 'Something went wrong!'
+		})
+	}
+}
+
+export const login = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body
+
+		if (!email || !password) {
+			return res.sendStatus(400).json({
+				success: false,
+				message: 'Some fields are missing.'
+			})
+		}
+
+		const user = await getUserByEmail(email).select(
+			'+authentication.salt +authentication.password'
+		)
+		if (!user) {
+			return res.sendStatus(400).json({
+				success: false,
+				message: 'No user registered with that email. Register first.'
+			})
+		}
+
+		const expectedHash = authentication(user.authentication.salt, password)
+
+		if (user.authentication.password !== expectedHash) {
+			return res.sendStatus(403).json({
+				success: false,
+				message: 'Unauthorized'
+			})
+		}
+
+		const salt = random()
+		user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+		await user.save()
+
+		res.cookie('REST_AUTH', user.authentication.sessionToken, {
+			domain: 'localhost',
+			path: '/'
+		})
+
+		return res.status(200).json(user).end()
 	} catch (error) {
 		console.log(error)
 		return res.sendStatus(400).json({
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import router from './router'
 
 const MONGO_URL = 'mongodb://127.0.0.1:9000'
 const app = express()
@@ -29,3 +30,5 @@ mongoose.Promise = Promise
 mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (err: Error) => console.log(err))
 mongoose.connection.on('connected', () => console.log('Connected to MongoDB'))
+
+app.use('/', router())
